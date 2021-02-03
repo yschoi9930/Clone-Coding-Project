a8df9fedf1b61655894982b6b6cc59673c5420fc
thtm111 <thtm11111@gmail.com>
2021-02-03 16:27:48 +0900

    Final-JCD

--- js/slideShow.js
+++ js/slideShow.js
@@ -1,54 +1,19 @@
 /**
-* slideShow.js
+* slideshow.js
 **/
 $(function(){
-
-    //이동한 이미지의  index 값 저장(현재보고있는 이미지-현재위치)
-    var movedIndex=0;
-
-    //슬라이드 패널을 움직여주는 함수
-   function moveSlide(index){
-        //전달받은 index 값을 movedIndex에 저장
-        movedIndex = index; //전역변수
-        //슬라이드 이동
-        var moveLeft = -(index * 1366);
-        $('#slidePanel').animate({'left':moveLeft},'slow');
-    };
-
-    //prev 버튼 클릭하면 앞으로 이동
-    $('#prevButton').on('click',function(){
-            //버튼클릭시 보여줘야 할 이미지의  index 결정해서
-            if(movedIndex !=0) //0번 인덱스 이미지가 아니라면
-                movedIndex = movedIndex - 1; //현재 이미지 이전 이전이미지의 인덱스로 설정
-
-            //moveSlide() 호출
-            moveSlide(movedIndex);
-    });//prev 버튼 종료
-
-    //next버튼 클릭하면 뒤로 이동
-    $('#nextButton').on('click',function(){
-        if(movedIndex !=5) //마지막 이미지가 아니면
-            movedIndex = movedIndex + 1;//다음 이미지 인덱스
-        //함수 호출
-        moveSlide(movedIndex);
-
-    });//next버튼 종료
-
-    //각 컨트롤 버튼에 대해
-    $('.controlButton').each(function(index){
-        $(this).hover(
-            function(){ //마우스 올렸을때 이미지 변경
-                $(this).attr('src','image/controlButton2.png');
-            },
-            function() { //마우스를 뗐을때 이미지 변경
-                $(this).attr('src','image/controlButton1.png');
-            }
-        );
-
-        //클릭했을 때 현재 인덱스 값을 moveSlide() 함수에게 전달
-        $(this).on('click',function(){
-            moveSlide(index);
-        }); //on 종료
-    });//each 함수 종료
-
-});//$(function 종료)
+    $('#Button img:eq(1)').on('click',function(){
+        $('#Button img:eq(1)').attr('src','image/controlButton2.png');
+        $('#Button img:eq(0)').attr('src','image/controlButton1.png');
+        $('#MainPhoto').animate({'left':-1900},0);
+        $('#TBox span').text('전국 스키장 어디든 할인돼요.');
+        $('#Appendix').text('리프트권부터 장비 렌탈까지');
+    });
+    $('#Button img:eq(0)').on('click',function(){
+        $('#Button img:eq(0)').attr('src','image/controlButton2.png');
+        $('#Button img:eq(1)').attr('src','image/controlButton1.png');
+        $('#MainPhoto').animate({'left':0},0);
+        $('#TBox span').text('낭만 가득한 겨울 캠핑을 만나보세요.');
+        $('#Appendix').text('감성 차박 패키지');
+    });
+});
